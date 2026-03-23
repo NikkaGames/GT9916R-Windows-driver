08003826919bbcff03a813d575a605622560710a
NikkaGames <nikagames111@gmail.com>
2026-03-23 22:58:25 +0400

    touchscreen: rename package to goodix9916r

--- build_arm64.cmd
+++ build_arm64.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=arm64
-msbuild "%~dp0vhidmini2.sln" /m /t:Build /p:Configuration=Release /p:Platform=ARM64 /p:GenerateFullPaths=true /v:m
+msbuild "%~dp0goodix9916r.sln" /m /t:Build /p:Configuration=Release /p:Platform=ARM64 /p:GenerateFullPaths=true /v:m
 exit /b %errorlevel%
